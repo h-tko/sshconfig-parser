45962bd694ee0bd61bc2cd9902aea4923451e2df
h-takeo <hiroaki.takeo@leverages.jp>
2017-03-15 19:05:06 +0900

    テストをGoConveyに

--- parse_test.go
+++ parse_test.go
@@ -2,20 +2,44 @@ package sshconfig
 
 import (
     "testing"
+    . "github.com/smartystreets/goconvey/convey"
 )
 
-func TestParse(t *testing.T) {
-    result, err := Parse("./.sshconfig_test")
+func TestParseFromConfig(t *testing.T) {
 
-    if err != nil {
-        t.Errorf("%v", err)
-    }
+    Convey("ただしいconfigファイルを読み込ませるとスライスに格納される", t, func() {
 
-    for _, data := range result {
-        t.Logf("host=%s", data.Host)
-        t.Logf("hostname=%s", data.HostName)
-        t.Logf("port=%d", data.Port)
-        t.Logf("user=%s", data.User)
-        t.Log("=================")
-    }
+        assumptions := []SSHConfig{
+            {
+                Host: "st2",
+                HostName: "st2.dev",
+                User: "development",
+                Port: 22,
+            },
+            {
+                Host: "test1",
+                HostName: "192.168.0.112",
+                User: "development",
+                Port: 19122,
+            },
+            {
+                Host: "test3",
+                HostName: "test.serve",
+                User: "user",
+                IdentityFile: "~/Downloads/test.pem",
+            },
+        }
+
+        result, err := Parse("./.sshconfig_test")
+
+        So(err, ShouldEqual, nil)
+
+        for index, data := range result {
+            So(data.Host, ShouldEqual, assumptions[index].Host)
+            So(data.HostName, ShouldEqual, assumptions[index].HostName)
+            So(data.Port, ShouldEqual, assumptions[index].Port)
+            So(data.User, ShouldEqual, assumptions[index].User)
+            So(data.IdentityFile, ShouldEqual, assumptions[index].IdentityFile)
+        }
+    })
 }
